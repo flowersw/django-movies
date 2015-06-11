0b7871826797ae7c0184454c91c2600d246d8f06
Will Flowers <will.flwrs@gmail.com>
2015-06-11 01:37:49 -0400

    Committing prelimnary work

--- movieratings/movieratings/views.py
+++ movieratings/movieratings/views.py
@@ -1,1 +1,10 @@
 __author__ = 'willflowers'
+
+from django.shortcuts import render
+from .models import Status
+from django.http import HttpResponse
+
+
+def all_statuses(request):
+    statuses = Status.objects.all()
+    return HttpResponse("Here are all the statuses.")
