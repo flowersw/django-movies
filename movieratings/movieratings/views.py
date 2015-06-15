b232abcd1e9037c13858e7300346df14df57ebc9
Will Flowers <will.flwrs@gmail.com>
2015-06-15 02:18:40 -0400

    committing what I have (not much)

--- movieratings/movieratings/views.py
+++ movieratings/movieratings/views.py
@@ -1,10 +1,40 @@
 __author__ = 'willflowers'
 
-from django.shortcuts import render
-from .models import Status
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+# Create your views here.
+from users.forms import UserForm, ProfileForm
 
 
-def all_statuses(request):
-    statuses = Status.objects.all()
-    return HttpResponse("Here are all the statuses.")
+def user_register(request):
+    if request.method == "GET":
+        user_form = UserForm()
+        profile_form = ProfileForm()
+    elif request.method == "POST":
+        user_form = UserForm(request.POST)
+        profile_form = ProfileForm(request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+
+            password = user.password
+            # The form doesn't know to call this special method on user.
+            user.set_password(password)
+            user.save()
+
+            # You must call authenticate before login. :(
+            user = authenticate(username=user.username,
+                                password=password)
+            login(request, user)
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                "Congratulations, {}, on creating your new account! You are now logged in.".format(
+                    user.username))
+            return redirect('index')
+    return render(request, "users/register.html", {'user_form': user_form,
+                                                   'profile_form': profile_form})
